e590afef7a0709034acd7f74acf99182e922a079
Benjamin Kyd <benjaminkyd@gmail.com>
2022-06-03 19:26:30 +0100

    olcpge

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
 
+#define OLC_PGE_APPLICATION
+#include <olcPixelGameEngine.h>
+
+class Game : public olc::PixelGameEngine
+{
+public:
+    Game()
+    {
+
+    }
+
+    ~Game()
+    {
+
+    }
+
+    bool OnUserCreate() override
+    {
+        return true;
+    }
+
+    bool OnUserUpdate( float fElapsedFrameTime ) override
+    {
+        Clear(olc::BLACK);
+
+        DrawRect( { 100, 100 }, { 10, 10 }, olc::RED );
+
+        return true;
+    }
+
+    bool OnUserDestroy() override
+    {
+        return true;
+    }
+
+};
+
+
 int main(int* argc, char** argv) 
 {
-    std::cout << "Hello, World!" << std::endl;
+    Game aRogueLike;
+
+    aRogueLike.Construct( 980, 720, 1, 1, false, false );
+    aRogueLike.Start();
+
     return 0;
 }
