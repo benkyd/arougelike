63f735dede8be149820acd4b9624e25005c917b3
Jake <jake@jaketbailey.co.uk>
2022-06-07 13:44:24 +0100

    figuring this out

--- src/main.cpp
+++ src/main.cpp
@@ -22,30 +22,48 @@ public:
 
     bool OnUserCreate() override
     {
+        fOffsetX = -ScreenWidth() / 2;
+        fOffsetY = -ScreenHeight() / 2;
         return true;
-    }
-
-    void UpdateWorld(float fWorldOffsetX, float fWorldOffsetY)
-    {
-        fWorldX = fWorldOffsetX;
-        fWorldY = fWorldOffsetX;
     }
 
     bool OnUserUpdate( float fElapsedFrameTime ) override
     {
         Clear(olc::BLACK);
+
+        float fMouseX = (float) GetMouseX();
+        float fMouseY = (float) GetMouseY();
+
+        if(GetMouse(0).bPressed)
+        {
+            fStartPanX = fMouseX;
+            fStartPanY = fMouseY;
+        }
+
+        if(GetMouse(0).bHeld)
+        {
+            fOffsetX -= (fMouseX - fStartPanX);
+            fOffsetY -= (fMouseY - fStartPanY);
+
+            fStartPanX = fMouseX;
+            fStartPanY = fMouseY;
+            std::cout << 'X';
+            std::cout << fStartPanX << std::endl;
+            std::cout << 'Y';
+            std::cout << fStartPanY << std::endl;
+        }
         
-        if (GetKey(olc::Key::A).bPressed) fStartPanX = fWorldX;
-        if (GetKey(olc::Key::D).bPressed) fStartPanX = fWorldX;
-        if (GetKey(olc::Key::W).bPressed) fStartPanY = fWorldY;
-        if (GetKey(olc::Key::S).bPressed) fStartPanY = fWorldY;
-
-        if (GetKey(olc::Key::A).bHeld) fOffsetX = fWorldX - 1;
-        if (GetKey(olc::Key::D).bHeld) fOffsetX = fWorldX + 1;
-        if (GetKey(olc::Key::W).bHeld) fOffsetY = fWorldY - 1;
-        if (GetKey(olc::Key::S).bHeld) fOffsetY = fWorldY + 1;
-
-        UpdateWorld(fOffsetX, fOffsetY);
+//        if (GetKey(olc::Key::A).bPressed) fStartPanX = fWorldX;
+//        if (GetKey(olc::Key::D).bPressed) ;
+//        if (GetKey(olc::Key::W).bPressed) fStartPanY = fWorldY;
+//        if (GetKey(olc::Key::S).bPressed) fStartPanY = fWorldY;
+//
+//        if (GetKey(olc::Key::A).bHeld) fOffsetX = fWorldX - 1;
+//        if (GetKey(olc::Key::D).bHeld) fOffsetX = fWorldX + 1;
+//        if (GetKey(olc::Key::W).bHeld) fOffsetY = fWorldY - 1;
+//        if (GetKey(olc::Key::S).bHeld) fOffsetY = fWorldY + 1;
+//
+//        UpdateWorld(fOffsetX, fOffsetY);
 
         mDungeon->Update();
         mDungeon->Draw();
@@ -63,21 +81,27 @@ private:
 
     float fCameraPos = 20.0f;
 
-    float fCenterX = 0 + (ScreenWidth() / static_cast<float>(2));
-    float fCenterY = 0 + (ScreenHeight() / static_cast<float>(2));
-
-    float fWorldX = fCenterX;
-    float fWorldY = fCenterY;
-
     float fStartPanX = 0.0f;
     float fStartPanY = 0.0f;
 
     float fOffsetX = 0.0f;
     float fOffsetY = 0.0f;
+
+    void WorldToScreen(float fWorldX, float fWorldY, int &nScreenX, int &nScreenY)
+    {
+        nScreenX = (int)(fWorldX) - fOffsetX;
+        nScreenY = (int)(fWorldY) - fOffsetY;
+    }
+
+    void ScreenToWorld(int nScreenX, int nScreenY, float &fWorldX, float &fWorldY)
+    {
+        fWorldX = (float)(nScreenX) + fOffsetX;
+        fWorldY = (float)(nScreenY) + fOffsetY;
+    }
 };
 
 
-int main(int* argc, char** argv) 
+int main(int argc, char** argv)
 {
     Game aRogueLike;
     
