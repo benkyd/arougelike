c1c85c603eed1227a813b34496b5ac2121130803
Jake Bailey - UP2002753 <jake.bailey2801@gmail.com>
2022-06-07 08:48:58 +0100

    so this doesn't work yet

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,27 @@ public:
         return true;
     }
 
+    void UpdateWorld(float fWorldOffsetX, float fWorldOffsetY)
+    {
+        fWorldX = fWorldOffsetX;
+        fWorldY = fWorldOffsetX;
+    }
+
     bool OnUserUpdate( float fElapsedFrameTime ) override
     {
         Clear(olc::BLACK);
+        
+        if (GetKey(olc::Key::A).bPressed) fStartPanX = fWorldX;
+        if (GetKey(olc::Key::D).bPressed) fStartPanX = fWorldX;
+        if (GetKey(olc::Key::W).bPressed) fStartPanY = fWorldY;
+        if (GetKey(olc::Key::S).bPressed) fStartPanY = fWorldY;
+
+        if (GetKey(olc::Key::A).bHeld) fOffsetX = fWorldX - 1;
+        if (GetKey(olc::Key::D).bHeld) fOffsetX = fWorldX + 1;
+        if (GetKey(olc::Key::W).bHeld) fOffsetY = fWorldY - 1;
+        if (GetKey(olc::Key::S).bHeld) fOffsetY = fWorldY + 1;
+
+        UpdateWorld(fOffsetX, fOffsetY);
 
         mDungeon->Update();
         mDungeon->Draw();
@@ -42,6 +60,20 @@ public:
 
 private:
     Dungeon* mDungeon;
+
+    float fCameraPos = 20.0f;
+
+    float fCenterX = 0 + (ScreenWidth() / static_cast<float>(2));
+    float fCenterY = 0 + (ScreenHeight() / static_cast<float>(2));
+
+    float fWorldX = fCenterX;
+    float fWorldY = fCenterY;
+
+    float fStartPanX = 0.0f;
+    float fStartPanY = 0.0f;
+
+    float fOffsetX = 0.0f;
+    float fOffsetY = 0.0f;
 };
 
 
